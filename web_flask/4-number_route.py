27d24e8c22680904dcbda87a3de4b0d198ee9d7f
Shina <m.atete@alustudent.com>
2025-06-10 09:00:33 +0200

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Starts the web app"""
+from flask import Flask
+from urllib.parse import unquote
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays the Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Returns the HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """Replace underscores with spaces"""
+    text = unquote(text).replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text):
+    """Replace underscores with spaces"""
+    text = unquote(text).replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """Display a number only if it is an integer"""
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
